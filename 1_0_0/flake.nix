d82e9b416431a872cb1a921de60112743ac25225
hugosenari <hugosenari@gmail.com>
2022-03-17 18:03:14 -0300

    chore: re-index ../nimpkgs/b/base45/1_0_0

--- 1_0_0/flake.nix
+++ 1_0_0/flake.nix
@@ -11,7 +11,7 @@
   inputs.src-base45-1_0_0.owner = "~ehmry";
   inputs.src-base45-1_0_0.ref   = "refs/tags/1.0.0";
   inputs.src-base45-1_0_0.repo  = "base45";
-  inputs.src-base45-1_0_0.type  = "other";
+  inputs.src-base45-1_0_0.type  = "sourcehut";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
